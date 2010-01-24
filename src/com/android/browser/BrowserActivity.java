047b9e305836a37a6ccc37657eecd9d287efd8b2
Steve Kondik <shade@chemlab.org>
2010-01-24 16:59:15 -0500

    Don't reenable the zoom controls after a pinch operation.

--- src/com/android/browser/BrowserActivity.java
+++ src/com/android/browser/BrowserActivity.java
@@ -1720,9 +1720,7 @@ public class BrowserActivity extends Activity
         int targetZoom = (int) Math.round(Math.log(newRelativeScale) * ZOOM_LOG_BASE_INV);
         final TabControl.Tab currentTab = mTabControl.getCurrentTab();
         WebView webView = currentTab.getWebView();
-        
-        webView.getSettings().setBuiltInZoomControls(false);
-        
+                
         if (mCurrZoom > targetZoom) {
         	while (mCurrZoom > targetZoom) {
         		mCurrZoom--;
@@ -1738,9 +1736,7 @@ public class BrowserActivity extends Activity
         		}
         	}
         }
-        
-        webView.getSettings().setBuiltInZoomControls(true);
-                
+                        
         return true;
     }
 
