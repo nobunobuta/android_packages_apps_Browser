31d9c48fd23108433937fd381633faef7978a69f
Steve Kondik <shade@chemlab.org>
2010-01-31 15:41:29 -0500

    Fix build for Donut.

--- src/com/android/browser/BrowserSettings.java
+++ src/com/android/browser/BrowserSettings.java
@@ -545,8 +545,8 @@ class BrowserSettings extends Observable {
             }
         });
 
-        GeolocationPermissions.getInstance().getOrigins(new ValueCallback<Set<String> >() {
-            public void onReceiveValue(Set<String> geolocationOrigins) {
+        GeolocationPermissions.getInstance().getOrigins(new ValueCallback<Set>() {
+            public void onReceiveValue(Set geolocationOrigins) {
                 if ((geolocationOrigins != null) && !geolocationOrigins.isEmpty()) {
                     screen.setEnabled(true);
                 }
