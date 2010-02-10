18cb7f353f2fe172114e3516bf3e40dbac477f1f
Steve Kondik <shade@chemlab.org>
2010-02-10 15:19:38 -0500

    Revert "Add Multitouch support using new API."

--- Android.mk
+++ Android.mk
@@ -4,7 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := user
 
 LOCAL_STATIC_JAVA_LIBRARIES := google-framework
-LOCAL_JAVA_LIBRARIES := com.cyanogenmod.android
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
